493da67678d2a8115712d3ed9137ae4adcb27852
DH0326 <daehyeonkim0326@gmail.com>
2026-06-07 23:04:41 +0900

    ssss

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Contact from "./pages/Contact";
 
 function App() {
   return (
-      <BrowserRouter basename="portfolio">
+      <BrowserRouter basename="/portfolio">
           <Routes>
             <Route element={<Layout/>}>
               <Route path="/"element={<Home/>}/>
